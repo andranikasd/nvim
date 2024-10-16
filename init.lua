b18dd2401af5095522306caf5e6a59a366c590d5
Andranik Grigoryan <andranik.grigoryan@powerdmarc.com>
2024-10-16 12:13:23 +0400

    feat: base-configuration
    1) Moved configuration files under `config` dir
    2) Setup of colortheme (Not works for now)

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
-require('bases.settings')
-require('bases.mappings')
+require('config.settings')
+require('config.mappings')
 require('config.lazy')
+
+
 -- Checking for compatible version
 if vim.fn.has('nvim-0.8') == 0 then
   error('Need Neovim 0.8+ in order to use this config')
--- lua/plugins/colorscheme.lua
+++
@@ -1,8 +0,0 @@
-{
-    "vague2k/huez.nvim",
-    branch = "stable",
-    event = "UIEnter",
-    config = function()
-        require("huez").setup({})
-    end,
-}

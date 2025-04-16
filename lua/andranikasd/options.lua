25c812d8c22ef26e1c1b42613ec254a2ba3b394a
Andranik Grigoryan <andranik.grigoryan.personal@gmail.com>
2025-04-17 00:22:32 +0400

    Commented wsl setup

--- lua/andranikasd/options.lua
+++ lua/andranikasd/options.lua
@@ -11,16 +11,16 @@
 -- ------------------------------------------------------------
 -- 1. CORE SETTINGS
 -- ------------------------------------------------------------
-vim.g.mapleader = " "             -- Set leader to space
-vim.opt.number = true             -- Show line numbers
-vim.opt.relativenumber = true    -- Relative line numbers
-vim.opt.tabstop = 2              -- Spaces per tab
-vim.opt.shiftwidth = 2           -- Indentation width
-vim.opt.expandtab = true         -- Convert tabs to spaces
-vim.opt.hidden = true            -- Allow background buffers
-vim.opt.splitright = true        -- Vertical splits open right
-vim.opt.splitbelow = true        -- Horizontal splits open below
-vim.opt.cursorline = true       -- Cursore current line highlight
+vim.g.mapleader = " "         -- Set leader to space
+vim.opt.number = true         -- Show line numbers
+vim.opt.relativenumber = true -- Relative line numbers
+vim.opt.tabstop = 2           -- Spaces per tab
+vim.opt.shiftwidth = 2        -- Indentation width
+vim.opt.expandtab = true      -- Convert tabs to spaces
+vim.opt.hidden = true         -- Allow background buffers
+vim.opt.splitright = true     -- Vertical splits open right
+vim.opt.splitbelow = true     -- Horizontal splits open below
+vim.opt.cursorline = true     -- Cursore current line highlight
 
 -- Enable filetype detection
 vim.cmd('filetype plugin indent on')
@@ -29,47 +29,48 @@ vim.cmd('syntax enable')
 -- ------------------------------------------------------------
 -- 2. ENHANCED CLIPBOARD CONFIGURATION (WSL)
 -- ------------------------------------------------------------
-local function setup_wsl_clipboard()
-    local win32yank_path = "/mnt/c/tools/win32yank.exe"
-    
-    -- Validate win32yank existence
-    if vim.fn.executable(win32yank_path) ~= 1 then
-        vim.notify("win32yank not found at: " .. win32yank_path, vim.log.levels.WARN)
-        return
-    end
-
-    -- Configure system clipboard
-    vim.g.clipboard = {
-        name = "WslClipboard",
-        copy = {
-            ["+"] = win32yank_path.." -i --crlf",
-            ["*"] = win32yank_path.." -i --crlf",
-        },
-        paste = {
-            ["+"] = win32yank_path.." -o --lf",
-            ["*"] = win32yank_path.." -o --lf",
-        },
-        cache_enabled = 0,
-    }
-
-    -- Custom yank mappings
-    local yank_cmd = win32yank_path.." -i --crlf"
-    
-    -- Visual mode yank to Windows clipboard
-    vim.keymap.set("v", "<leader>y", function() 
-        vim.cmd(string.format("'<,'>w !%s", yank_cmd))
-    end, { desc = "Yank selection to Windows clipboard" })
-
-    -- Normal mode yank line to Windows clipboard
-    vim.keymap.set("n", "<leader>Y", function()
-        vim.cmd(string.format(".w !%s", yank_cmd))
-    end, { desc = "Yank line to Windows clipboard" })
-end
+-- local function setup_wsl_clipboard()
+--   local win32yank_path = "/mnt/c/tools/win32yank.exe"
+-- 
+--   -- Validate win32yank existence
+--   if vim.fn.executable(win32yank_path) ~= 1 then
+--     vim.notify("win32yank not found at: " .. win32yank_path, vim.log.levels.WARN)
+-- 
+--     return
+--   end
+-- 
+--   -- Configure system clipboard
+--   vim.g.clipboard = {
+--     name = "WslClipboard",
+--     copy = {
+--       ["+"] = win32yank_path .. " -i --crlf",
+--       ["*"] = win32yank_path .. " -i --crlf",
+--     },
+--     paste = {
+--       ["+"] = win32yank_path .. " -o --lf",
+--       ["*"] = win32yank_path .. " -o --lf",
+--     },
+--     cache_enabled = 0,
+--   }
+-- 
+--   -- Custom yank mappings
+--   local yank_cmd = win32yank_path .. " -i --crlf"
+-- 
+--   -- Visual mode yank to Windows clipboard
+--   vim.keymap.set("v", "<leader>y", function()
+--     vim.cmd(string.format("'<,'>w !%s", yank_cmd))
+--   end, { desc = "Yank selection to Windows clipboard" })
+-- 
+--   -- Normal mode yank line to Windows clipboard
+--   vim.keymap.set("n", "<leader>Y", function()
+--     vim.cmd(string.format(".w !%s", yank_cmd))
+--   end, { desc = "Yank line to Windows clipboard" })
+-- end
 
 -- Only configure for WSL
-if vim.fn.has("wsl") == 1 then
-    setup_wsl_clipboard()
-end
+-- if vim.fn.has("wsl") == 1 then
+--   setup_wsl_clipboard()
+-- end
 -- ------------------------------------------------------------
 -- 3. KEYBINDINGS
 -- ------------------------------------------------------------
@@ -110,5 +111,5 @@ keymap('n', '<leader>-', ':resize -5<CR>', opts)
 keymap('n', '<leader>>', ':vertical resize +5<CR>', opts)
 keymap('n', '<leader><', ':vertical resize -5<CR>', opts)
 
--- Some env setup 
+-- Some env setup
 vim.env.PATH = vim.env.PATH .. ':' .. vim.fn.stdpath('data') .. '/mason/bin'
